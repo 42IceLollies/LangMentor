5cc54b3426c9702f41685de5e2bd04f9e5f953cf
SoftwareFuze <atticus@softwarefuze.com>
2021-06-13 07:03:16 -0700

    Setup backend

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+// other setup
+require("dotenv").config();
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.static('public'));
+
+// main API
+app.get('/', (req, res) => {
+	res.json({ status: 200, message: "Welcome to the LangMentor ReST API" });
+});
+
+// start server
+app.listen(process.env.PORT || 8000, () => console.log('Server running...'));
